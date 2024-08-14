298853a0f455d960b31b2ab4278faea3b185d761
IsoqulovBekzod <isoqulovbekzod6@gmail.com>
2024-08-14 18:47:01 +0500

    Vazifa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["public/**/*.{html,js,php"],
+  content: ["./public/**/*{js,jsx,ts,tsx,php}"],
   theme: {
     extend: {},
   },

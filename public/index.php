e8d785cad620a1c9f27fb4140db4b970dc1b998d
IsoqulovBekzod <isoqulovbekzod6@gmail.com>
2024-08-15 10:33:08 +0500

    Vazifa

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
+<?php
+require "../bootstrap.php";
+require "../functions.php";
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
---
+++ src/Branch.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App;
+use PDO;
+
+class Branch{
+    private PDO  $pdo;
+    public function __construct(){
+        $this->pdo = DB::connect();
+    }
+    public function create(
+        string  $name,
+        string  $address,
+    ){
+        $query = "INSERT INTO branch( name, address,created_at) VALUES ( :name, :address, NOW())";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':address', $address);
+        $stmt->execute();
+    }
+
+    public function getBranch(int $id){
+        $query = "SELECT * FROM branch WHERE id = :id";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function updateBranch(
+        int     $id,
+        string  $name,
+        string  $address,
+    ){
+        $query = "UPDATE branch SET name = :name, address = :address, update_at = NOW() WHERE id = :id";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':address', $address);
+        $stmt->execute();
+    }
+
+    public function deleteBranch(int $id):void{
+        $query = "DELETE FROM branch WHERE id = :id";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+    }
+
+
+
+
+
+}
